3d4dcfb92b9a3c09e2dac8dd780b77a4e913bad6
sw8744 <sw8744@naver.com>
2024-03-05 23:21:13 +0900

    Fix Bugs

--- backend/main.py
+++ backend/main.py
@@ -105,59 +105,53 @@ def updown():
     capture.release()
     cv2.destroyAllWindows()
 
-@app.route('/api/updown', methods=['GET'])
-def updown():
-    # FIXME: Arduino와의 연동 필요
-    prev_pos = ""
-    capture = cv2.VideoCapture(0)
-    capture.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-    capture.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
-    ageProto = 'age_deploy.prototxt'
-    ageModel = 'age_net.caffemodel'
-    MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
-    ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
-    print("Camera_Connected")
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-    while True:
-        ret, frame = capture.read()
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, 1.3, 5, minSize=(20, 20))
-        x_f = 0; y_f = 0; w_f = 0; h_f = 0
-        for (x, y, w, h) in faces:
-            x_f = x; y_f = y; w_f = w; h_f = h
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-            cv2.circle(frame, (x + w // 2, y + h // 2), 5, (0, 0, 255), -1)
-            # print(x, y, w, h)
-        if len(faces) > 0:
-            if y_f + h_f // 2 < height // 3:
-                if prev_pos != "up":
-                    prev_pos = "up"
-                    print("up")
-            elif y_f + h_f // 2 < height // 3 * 2:
-                print("middle")
-                time.sleep(0.5)
-                for (x, y, w, h) in faces:
-                    face_img = frame[y:y, h:h + w].copy()
-                    blob = cv2.dnn.blobFromImage(frame, 1.0, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
-                    ageNet = cv2.dnn.readNet(ageModel, ageProto)
-                    ageNet.setInput(blob)
-                    agePreds = ageNet.forward()
-                    age = ageList[agePreds[0].argmax()]
-                    return make_response(jsonify({"age": age[1:-1]}), 200)
-                return make_response(jsonify({"result": "success"}), 200)
-            else:
-                if prev_pos != "down":
-                    prev_pos = "down"
-                    print("down")
-        elif len(faces) == 0:
-            if prev_pos != "error":
-                prev_pos = "error"
-                print("Please see the camera correctly")
-        cv2.imshow('frame', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    capture.release()
-    cv2.destroyAllWindows()
+    @app.route('/api/updown', methods=['POST'])
+    def updown():
+        # FIXME: Arduino와의 연동 필요
+        prev_pos = ""
+        capture = cv2.VideoCapture(0)
+        capture.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+        capture.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+        print("Camera_Connected")
+        face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+        while True:
+            ret, frame = capture.read()
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(gray, 1.3, 5, minSize=(20, 20))
+            x_f = 0;
+            y_f = 0;
+            w_f = 0;
+            h_f = 0
+            for (x, y, w, h) in faces:
+                x_f = x;
+                y_f = y;
+                w_f = w;
+                h_f = h
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+                cv2.circle(frame, (x + w // 2, y + h // 2), 5, (0, 0, 255), -1)
+                # print(x, y, w, h)
+            if len(faces) > 0:
+                if y_f + h_f // 2 < height // 3:
+                    if prev_pos != "up":
+                        prev_pos = "up"
+                        print("up")
+                elif y_f + h_f // 2 < height // 3 * 2:
+                    print("middle")
+                    return make_response(jsonify({"result": "success"}), 200)
+                else:
+                    if prev_pos != "down":
+                        prev_pos = "down"
+                        print("down")
+            elif len(faces) == 0:
+                if prev_pos != "error":
+                    prev_pos = "error"
+                    print("Please see the camera correctly")
+            cv2.imshow('frame', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        capture.release()
+        cv2.destroyAllWindows()
+
 
 @app.route('/api/pay', methods=['POST'])
 def pay():
