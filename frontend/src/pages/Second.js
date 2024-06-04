eded7840e9520c698a1f1bac3d703c89a025b172
thislife_won <sw8744@naver.com>
2024-06-04 21:50:46 +0900

    Edit Links in Second.js

--- frontend/src/pages/Second.js
+++ frontend/src/pages/Second.js
@@ -1,20 +1,32 @@
 import './Second.css';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 function Second() {
     const navigate = useNavigate();
-    const takeOut = () => {
-        navigate('/menu?whereToEat=포장');
+
+    const fetchWhereToEat = async (whereToEat) => {
+        let result = await fetch('http://127.0.0.1:5000/api/start/eatIn').then(response => response.json());
+        console.log(result)
+        return result.order_num;
+    }
+
+    const takeOut = async () => {
+        let ordernum = await fetchWhereToEat('takeOut');
+        navigate('/menu?id=' + ordernum);
     };
-    const eatIn = () => {
-        navigate('/menu?whereToEat=매장');
+    const eatIn = async () => {
+        let ordernum = await fetchWhereToEat('eatIn');
+        navigate('/menu?id=' + ordernum);
     };
+
     return (
         <div className='total'>
             <button className='takeOut' onClick={takeOut}>포장</button><br/>
             <button className='eatIn' onClick={eatIn}>매장</button>
         </div>
     );
-}
+    }
+
 
 export default Second;
