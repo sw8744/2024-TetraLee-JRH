18598fdb2c8a2406d79de64c2cfd67e9ed998824
thislife_won <sw8744@naver.com>
2024-07-08 19:23:06 +0900

    Change post to get

--- frontend/src/pages/First.js
+++ frontend/src/pages/First.js
@@ -4,7 +4,8 @@ import { useNavigate } from 'react-router-dom';
 function First() {
     const navigate = useNavigate();
     const touch = () => {
-        fetch('http://127.0.0.1:5000/api/updown', {method: 'POST'});
+        fetch('http://127.0.0.1:5000/api/updown')
+        .then(response => response.json());
         navigate('/wheretoeat');
     }
     return (
