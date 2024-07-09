b32e082322bedb163b244632973542ef6a873bad
thislife_won <sw8744@naver.com>
2024-07-09 18:44:35 +0900

    Edit main.py

--- frontend/src/pages/First.js
+++ frontend/src/pages/First.js
@@ -1,21 +1,13 @@
-import { useEffect } from 'react';
 import './First.css';
 import { useNavigate } from 'react-router-dom';
-import { getSpeech } from '../util/GetSpeech';
 
 function First() {
     const navigate = useNavigate();
     const touch = () => {
         fetch('http://127.0.0.1:5000/api/updown')
         .then(response => response.json());
-        getSpeech('사용자의 키를 감지해 높이를 변경하는 중입니다. 잠시만 기다려주세요!')
         navigate('/wheretoeat');
     }
-
-    useEffect(() => {
-        window.speechSynthesis.getVoices();
-    }, []);
-
     return (
         <>
             <div className='root'>
--- frontend/src/pages/Second.js
+++ frontend/src/pages/Second.js
@@ -1,11 +1,8 @@
 import './Second.css';
 import { useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
-import { getSpeech } from '../util/GetSpeech';
 
 function Second() {
     const navigate = useNavigate();
-    const [load, setLoad] = useEffect(false);
 
     const fetchWhereToEat = async (whereToEat) => {
         let result = await fetch('http://127.0.0.1:5000/api/start/' + whereToEat)
@@ -21,15 +18,6 @@ function Second() {
         let ordernum = await fetchWhereToEat('eatIn');
         navigate('/menu?id=' + ordernum);
     };
-
-    useEffect(() => {
-        window.speechSynthesis.getVoices();
-        setLoad(true);
-    }, []);
-
-    useEffect(() => {
-        getSpeech('포장하실지, 매장에서 드실지 선택해주세요.');
-    }, [load]);
 
     return (
         <div className='total'>
